3e955516fee4cdbae8503daa58c40663199e8ddf
johncross00 <johncross.ktm@gmail.com>
2024-02-25 00:01:28 +0000

    invoices m.

--- database/migrations/2024_02_24_223120_create_invoices_table.php
+++ database/migrations/2024_02_24_223120_create_invoices_table.php
@@ -16,6 +16,10 @@ return new class extends Migration
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
             $table->integer('customer_id');
+            $table->integer('amount');
+            $table->string('status');
+            $table->dateTime('billed_dated');
+            $table->dateTime('paid_dated');
             $table->timestamps();
         });
     }
