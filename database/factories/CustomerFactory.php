8cd00a8001b00ca4fb1dc08d6b687465f29fdf0a
johncross00 <johncross.ktm@gmail.com>
2024-02-25 01:48:58 +0000

    .

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -16,8 +16,16 @@ class CustomerFactory extends Factory
      */
     public function definition()
     {
+        $type = $this->faker->randomElement(['I', 'B']);
+        $name = $type == 'I' ? $this->faker->name() : $this->faker->company();
         return [
-            //
+            'name' => $name,
+            'type' => $type,
+            'email' => $this->faker->email(),
+            'address' => $this->faker->streetAddress(),
+            'city' => $this->faker->city(),
+            'state' => $this->faker->state(),
+            'postalCode' => $this->faker->postcode()
         ];
     }
 }
